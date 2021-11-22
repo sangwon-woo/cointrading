b6093375bd0d5100541d3f6732411bf51e040634
SangwonWoo <riddlewoo713@gmail.com>
2021-11-23 00:59:22 +0900

    update

--- tests/test_korbit_machine.py
+++ tests/test_korbit_machine.py
@@ -1,12 +1,24 @@
 import unittest
+from unittest import async_case
 from autotrading.machine.korbit_machine import KorbitMachine
 import inspect
 
 class KorbitMachineTestCase(unittest.TestCase):
 
-    def setUp(self):
+    def __init__(self):
         self.korbit_machine = KorbitMachine()
 
     def tearDown(self):
         pass
     
+    def test_set_token(self):
+        print(inspect.stack()[0][3])
+        expire, access_token, refresh_token = self.korbit_machine.set_token(grant_type='password')
+        assert access_token
+        print('Expire:', expire, 'Access token:', access_token, 'Refresh token', refresh_token)
+
+
+if __name__ == '__main__':
+    print(__name__)
+    c = KorbitMachineTestCase()
+    c.test_set_token()
