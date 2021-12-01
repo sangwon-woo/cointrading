a2e1aa35d29cf8b3d1ed14a29a5b16888fba8dc2
SangwonWoo <riddlewoo713@gmail.com>
2021-12-01 16:48:50 +0900

    Update upbit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import configparser 
 import pybithumb
+import pyupbit
 
 parser = configparser.ConfigParser()
 parser.read('config/config.ini')
@@ -11,3 +12,9 @@ bithumb = pybithumb.Bithumb(connect_key, secret_key)
 
 balance = bithumb.get_balance('BTC')
 
+acc_key = parser.get('upbit', 'access_key')
+sec_key = parser.get('upbit', 'secret_key')
+
+upbit = pyupbit.Upbit(acc_key, sec_key)
+upbit_balance = upbit.get_balances()
+print(upbit_balance)
