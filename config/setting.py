7b39e826024fc59c3b4c1f1cf796415a2d4db679
SangwonWoo <riddlewoo713@gmail.com>
2021-12-07 11:52:45 +0900

    Update bithumb && coinone && korbit key

--- config/setting.py
+++ config/setting.py
@@ -5,6 +5,19 @@ parser.read('config/config.ini')
 
 UPBIT_ACCESS_KEY = parser.get('upbit', 'access_key')
 UPBIT_SECRET_KEY = parser.get('upbit', 'secret_key')
+UPBIT_DATABASE_DIR = 'D:\\coin_database\\domestic\\upbit'
+
+BITHUMB_CONNECT_KEY = parser.get('bithumb', 'connect_key')
+BITHUMB_SECRET_KEY = parser.get('bithumb', 'secret_key')
+BITHUMB_DATABASE_DIR = 'D:\\coin_database\\domestic\\bithumb'
+
+KORBIT_CLIENT_ID = parser.get('korbit', 'client_id')
+KORBIT_CLIENT_SECRET = parser.get('korbit', 'client_secret')
+KORBIT_DATABASE_DIR = 'D:\\coin_database\\domestic\\korbit'
+
+COINONE_ACCESS_TOKEN = parser.get('coinone', 'access_token')
+COINONE_SECRET_KEY = parser.get('coinone', 'secret_key')
+COINONE_DATABASE_DIR = 'D:\\coin_database\\domestic\\coinone'
 
 
 
