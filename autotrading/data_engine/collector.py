9c83c1d447b81e60e2e9a40248229da84e015ee0
SangwonWoo <riddlewoo713@gmail.com>
2021-12-17 01:42:31 +0900

    New file : collector.py

---
+++ autotrading/data_engine/collector.py
@@ -0,0 +1,40 @@
+import time
+import pandas as pd
+from config.setting import *
+
+class Collector:
+    def __init__(self, machine):
+        self.machine = machine()
+
+    def get_last_time(self, df):
+        last_time = df.loc[df.shape[0]-1, 'candle_date_time_utc']
+        last_time = ' '.join(last_time.split('T'))
+
+        return last_time
+        
+    def get_market_code(self):
+        market = self.machine.get_market_code()
+        krw_market_code = [c for c in market['market'].to_list() if c.startswith('KRW')]
+
+        return krw_market_code
+
+    def collect_minutely_all_data(self, markets:list):
+        
+        for market in markets:
+            s = time.time()
+            coin_df = pd.DataFrame()
+            save_dir = UPBIT_DATABASE_DIR + f'\\minutely\\{market}.csv'
+            df = self.machine.get_minute_candle(unit=1, market=market, count=200)
+
+            last_time = self.get_last_time(df)
+            coin_df = coin_df.append(df, ignore_index=True)
+            
+            while True:
+                df = self.machine.get_minute_candle(unit=1, market=market, to=last_time, count=200)
+                if len(df.index) == 0:
+                    break
+                last_time = self.get_last_time(df)
+                coin_df = coin_df.append(df, ignore_index=True)
+
+            coin_df.to_csv(save_dir, index=None, encoding='utf-8')
+            print(f'{market} => Delta', time.time() - s)
