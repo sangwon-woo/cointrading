416b768dfdc6a153a5ab549ccebcb5bf7a530feb
SangwonWoo <riddlewoo713@gmail.com>
2021-12-17 12:54:27 +0900

    Update collector.py

--- autotrading/data_engine/collector.py
+++ autotrading/data_engine/collector.py
@@ -24,10 +24,10 @@ class Collector:
             s = time.time()
             coin_df = pd.DataFrame()
             save_dir = UPBIT_DATABASE_DIR + f'\\minutely\\{market}.csv'
-            df = self.machine.get_minute_candle(unit=1, market=market, count=200)
+            latest_df = self.machine.get_minute_candle(unit=1, market=market, count=200)
 
-            last_time = self.get_last_time(df)
-            coin_df = coin_df.append(df, ignore_index=True)
+            last_time = self.get_last_time(latest_df)
+            coin_df = coin_df.append(latest_df, ignore_index=True)
             
             while True:
                 df = self.machine.get_minute_candle(unit=1, market=market, to=last_time, count=200)
@@ -38,3 +38,83 @@ class Collector:
 
             coin_df.to_csv(save_dir, index=None, encoding='utf-8')
             print(f'{market} => Delta', time.time() - s)
+
+    def collect_minutely_data_until_now(self, markets:list):
+
+        for market in markets:
+            s = time.time()
+            load_dir = UPBIT_DATABASE_DIR + f'\\minutely\\{market}.csv'
+            save_dir = UPBIT_DATABASE_DIR + f'\\minutely\\{market}.csv'
+
+            old_df = pd.read_csv(load_dir, encoding='utf-8')
+            first_time = old_df.loc[0, 'candle_date_time_utc']
+
+            latest_df = self.machine.get_minute_candle(unit=1, market=market, count=200)
+            last_time = self.get_last_time(latest_df)
+            
+            if last_time <= first_time:
+                subset_df = latest_df[latest_df['candle_date_time_utc'] > first_time]
+                old_df = old_df.append(subset_df, ignore_index=True)
+
+                return old_df
+            else:
+                while True:
+                    df = self.machine.get_minute_candle(unit=1, market=market, to=last_time)
+
+                    if last_time <= first_time:
+                        subset_df = df[df['candle_date_time_utc'] > first_time]
+                        old_df = old_df.append(subset_df, ignore_index=True)
+                        return old_df
+                    last_time = self.get_last_time(df)
+                    old_df = old_df.append(df, ignore_index=True)
+
+    def collect_daily_all_data(self, markets:list):
+        
+        for market in markets:
+            s = time.time()
+            coin_df = pd.DataFrame()
+            save_dir = UPBIT_DATABASE_DIR + f'\\daily\\{market}.csv'
+            latest_df = self.machine.get_minute_candle(unit=1, market=market, count=200)
+
+            last_time = self.get_last_time(latest_df)
+            coin_df = coin_df.append(latest_df, ignore_index=True)
+            
+            while True:
+                df = self.machine.get_minute_candle(unit=1, market=market, to=last_time, count=200)
+                if len(df.index) == 0:
+                    break
+                last_time = self.get_last_time(df)
+                coin_df = coin_df.append(df, ignore_index=True)
+
+            coin_df.to_csv(save_dir, index=None, encoding='utf-8')
+            print(f'{market} => Delta', time.time() - s)
+
+    def collect_daily_data_until_now(self, markets:list):
+
+        for market in markets:
+            s = time.time()
+            load_dir = UPBIT_DATABASE_DIR + f'\\daily\\{market}.csv'
+            save_dir = UPBIT_DATABASE_DIR + f'\\daily\\{market}.csv'
+
+            old_df = pd.read_csv(load_dir, encoding='utf-8')
+            first_time = old_df.loc[0, 'candle_date_time_utc']
+
+            latest_df = self.machine.get_minute_candle(unit=1, market=market, count=200)
+            last_time = self.get_last_time(latest_df)
+            
+            if last_time <= first_time:
+                subset_df = latest_df[latest_df['candle_date_time_utc'] > first_time]
+                old_df = old_df.append(subset_df, ignore_index=True)
+
+                return old_df
+            else:
+                while True:
+                    df = self.machine.get_minute_candle(unit=1, market=market, to=last_time)
+
+                    if last_time <= first_time:
+                        subset_df = df[df['candle_date_time_utc'] > first_time]
+                        old_df = old_df.append(subset_df, ignore_index=True)
+                        return old_df
+                    last_time = self.get_last_time(df)
+                    old_df = old_df.append(df, ignore_index=True)                
+
