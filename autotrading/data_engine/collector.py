236e5f69c130efa1db50e866054d03b6751dccd5
SangwonWoo <riddlewoo713@gmail.com>
2021-12-20 17:49:08 +0900

    Add set_columns_dtypes method

--- autotrading/data_engine/collector.py
+++ autotrading/data_engine/collector.py
@@ -1,5 +1,6 @@
 import time
 import pandas as pd
+import numpy as np
 from config.setting import *
 from dateutil.parser import parse
 
@@ -18,6 +19,51 @@ class Collector:
         krw_market_code = [c for c in market['market'].to_list() if c.startswith('KRW')]
 
         return krw_market_code
+
+    def set_columns_dtypes(self, df, type='daily'):
+        assert type in ['daily', 'minutely'], "daily와 minuetely 중 하나를 택하세요."
+
+        if type == 'daily':
+            columns = {
+                'market' : '시장-코인',
+                'candle_date_time_utc' : '시각_utc',
+                'candle_date_time_kst' : '시각_kst',
+                'opening_price' : '시가',
+                'high_price' : '고가',
+                'low_price' : '저가',
+                'trade_price' : '종가',
+                'candle_acc_trade_price' : '누적거래금액',
+                'candle_acc_trade_volume' : '누적거래량',
+                'change_price' : '전일종가대비변화금액',
+                'change_rate' : '전일종가대비변화량'
+            }
+            dtypes = {
+                '시장-코인' : 'category',
+                '누적거래금액' : np.uint64,
+                '누적거래량' : np.uint64
+            }
+            df = df.drop(columns=['timestamp', 'unit']).rename(columns=columns).astype(dtypes)
+            
+        elif type == 'minutely':
+            columns = {
+                'market' : '시장-코인',
+                'candle_date_time_utc' : '시각_utc',
+                'candle_date_time_kst' : '시각_kst',
+                'opening_price' : '시가',
+                'high_price' : '고가',
+                'low_price' : '저가',
+                'trade_price' : '종가',
+                'candle_acc_trade_price' : '누적거래금액',
+                'candle_acc_trade_volume' : '누적거래량'
+            }
+            dtypes = {
+                '시장-코인' : 'category',
+                '누적거래금액' : np.uint64,
+                '누적거래량' : np.unit64
+            }
+            df = df.drop(columns=['timestamp', 'unit']).rename(columns=columns).astype(dtypes)
+
+        return df
 
     def collect_minutely_all_data(self, markets:list):
         
@@ -136,6 +182,6 @@ class Collector:
                         old_df.to_csv(save_dir, index=None, encoding='utf-8')
                         print(f'{market} => Delta', time.time() - s)
                         continue
-                    
+
                     old_df = old_df.append(df, ignore_index=True)                
 
