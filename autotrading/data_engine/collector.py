cf1e039446e70bcf6f901a13d0eea5752f6a5fcb
SangwonWoo <riddlewoo713@gmail.com>
2021-12-21 15:44:21 +0900

    Update methods

--- autotrading/data_engine/collector.py
+++ autotrading/data_engine/collector.py
@@ -1,4 +1,5 @@
 import time
+from numpy.lib.type_check import imag
 import pandas as pd
 import numpy as np
 from config.setting import *
@@ -10,48 +11,54 @@ class Collector:
 
     def check_dtypes(self, df):
         col_dtypes = {}
+        list_int = ['int', 'int8', 'uint8', 'int16', 'uint16', 'int32', 'uint32', 'int64', 'uint64']
+        list_float = ['float', 'float32', 'float64']
+
         for col in df.columns:
             dtype = df[col].dtype
 
-            if dtype == 'int' or dtype == 'float':
+            if dtype in list_int or dtype in list_float:
                 c_min = df[col].min()
                 c_max = df[col].max()
+
+                if dtype in list_int:
+                    if c_min >= np.iinfo(np.int8).min and c_max <= np.iinfo(np.int8).max:
+                        col_dtype = 'int8'
+                    elif c_min >= np.iinfo(np.uint8).min and c_max <= np.iinfo(np.uint8).max:
+                        col_dtype = 'uint8'
+                    elif c_min >= np.iinfo(np.int16).min and c_max <= np.iinfo(np.int16).max:
+                        col_dtype = 'int16'
+                    elif c_min >= np.iinfo(np.uint16).min and c_max <= np.iinfo(np.uint16).max:
+                        col_dtype = 'uint16'
+                    elif c_min >= np.iinfo(np.int32).min and c_max <= np.iinfo(np.int32).max:
+                        col_dtype = 'int32'
+                    elif c_min >= np.iinfo(np.uint32).min and c_max <= np.iinfo(np.uint32).max:
+                        col_dtype = 'uint32'
+                    elif c_min >= np.iinfo(np.int64).min and c_max <= np.iinfo(np.int64).max:
+                        col_dtype = 'int64'
+                    elif c_min >= np.iinfo(np.uint64).min and c_max <= np.iinfo(np.uint64).max:
+                        col_dtype = 'uint64'
+
+                elif dtype in list_float:
+                    # if c_min >= np.finfo(np.float16).min and c_max <= np.finfo(np.float16).max:
+                    #     col_dtype = 'float16'
+                    if c_min >= np.finfo(np.float32).min and c_max <= np.finfo(np.float32).max:
+                        col_dtype = 'float32'
+                    elif c_min >= np.finfo(np.float64).min and c_max <= np.finfo(np.float64).max:
+                        col_dtype = 'float64'
+
             elif dtype == 'object':
                 n_unique = df[col].nunique()
                 threshold = n_unique / df.shape[0]
 
-            if dtype == 'int':
-                if c_min >= np.iinfo(np.int8).min and c_max <= np.iinfo(np.int8).max:
-                    col_dtype = 'int8'
-                elif c_min >= np.iinfo(np.uint8).min and c_max <= np.iinfo(np.uint8).max:
-                    col_dtype = 'uint8'
-                elif c_min >= np.iinfo(np.int16).min and c_max <= np.iinfo(np.int16).max:
-                    col_dtype = 'int16'
-                elif c_min >= np.iinfo(np.uint16).min and c_max <= np.iinfo(np.uint16).max:
-                    col_dtype = 'uint16'
-                elif c_min >= np.iinfo(np.int32).min and c_max <= np.iinfo(np.int32).max:
-                    col_dtype = 'int32'
-                elif c_min >= np.iinfo(np.uint32).min and c_max <= np.iinfo(np.uint32).max:
-                    col_dtype = 'uint32'
-                elif c_min >= np.iinfo(np.int64).min and c_max <= np.iinfo(np.int64).max:
-                    col_dtype = 'int64'
-                elif c_min >= np.iinfo(np.uint64).min and c_max <= np.iinfo(np.uint64).max:
-                    col_dtype = 'uint64'
-
-
-            elif dtype == 'float':
-                # if c_min >= np.finfo(np.float16).min and c_max <= np.finfo(np.float16).max:
-                #     col_dtype = 'float16'
-                if c_min >= np.finfo(np.float32).min and c_max <= np.finfo(np.float32).max:
-                    col_dtype = 'float32'
-                elif c_min >= np.finfo(np.float64).min and c_max <= np.finfo(np.float64).max:
-                    col_dtype = 'float64'
-
-            elif dtype == 'object':
                 if threshold > 0.7:
                     col_dtype = 'object'
                 else:
                     col_dtype = 'category'
+
+            elif dtype == 'category':
+                col_dtype = dtype
+
             col_dtypes[col] = col_dtype
 
         return col_dtypes
@@ -70,6 +77,11 @@ class Collector:
 
     def set_columns_dtypes(self, df, type='daily'):
         assert type in ['daily', 'minutely'], "daily와 minuetely 중 하나를 택하세요."
+
+        if df.columns[0] == '시장-코인':
+            dtypes = self.check_dtypes(df)
+            df = df.astype(dtypes)
+            return df
 
         if type == 'daily':
             columns = {
@@ -141,6 +153,7 @@ class Collector:
 
             old_df = pd.read_feather(load_dir)
             first_time = old_df.loc[0, '시각_utc']
+            old_df = old_df.loc[1:, :]
 
             latest_df = self.machine.get_minute_candle(unit=1, market=market, count=200)
             latest_df = self.set_columns_dtypes(latest_df, type='minutely')
@@ -149,29 +162,35 @@ class Collector:
             parsed_first_time, parsed_last_time = parse(first_time), parse(last_time)
             
             if parsed_last_time <= parsed_first_time:
-                subset_df = latest_df[latest_df['시각_utc'] > parsed_first_time]
+                subset_df = latest_df[latest_df['시각_utc'] >= first_time]
                 old_df = old_df.append(subset_df, ignore_index=True)
 
                 old_df = self.set_columns_dtypes(old_df, type='minutely')
+                old_df = old_df.sort_values('시각_utc', ascending=False).reset_index(drop=True)
+
                 old_df.to_feather(save_dir)
                 print(f'{market} => Delta', time.time() - s)
                 continue
 
             else:
+                old_df = old_df.append(latest_df, ignore_index=True)
+
                 while True:
-                    df = self.machine.get_minute_candle(unit=1, market=market, to=last_time)
+                    df = self.machine.get_minute_candle(unit=1, market=market, to=last_time, count=200)
                     df = self.set_columns_dtypes(df, type='minutely')
                     last_time = self.get_last_time(df)
                     parsed_last_time = parse(last_time)
 
                     if parsed_last_time <= parsed_first_time:
-                        subset_df = df[df['시각_utc'] > parsed_first_time]
+                        subset_df = df[df['시각_utc'] >= first_time]
                         old_df = old_df.append(subset_df, ignore_index=True)
                         
                         old_df = self.set_columns_dtypes(old_df, type='minutely')
+                        old_df = old_df.sort_values('시각_utc', ascending=False).reset_index(drop=True)
+
                         old_df.to_feather(save_dir)
                         print(f'{market} => Delta', time.time() - s)
-                        continue
+                        break
                     old_df = old_df.append(df, ignore_index=True)
 
     def collect_daily_all_data(self, markets:list):
@@ -206,6 +225,7 @@ class Collector:
 
             old_df = pd.read_feather(load_dir)
             first_time = old_df.loc[0, '시각_utc']
+            old_df = old_df.loc[1:, :]
 
             latest_df = self.machine.get_day_candle(market=market, count=200)
             latest_df = self.set_columns_dtypes(latest_df, type='daily')
@@ -214,29 +234,35 @@ class Collector:
             parsed_first_time, parsed_last_time = parse(first_time), parse(last_time)
             
             if parsed_last_time <= parsed_first_time:
-                subset_df = latest_df[latest_df['시각_utc'] > parsed_first_time]
+                subset_df = latest_df[latest_df['시각_utc'] >= first_time]
                 old_df = old_df.append(subset_df, ignore_index=True)
 
                 old_df = self.set_columns_dtypes(old_df, type='daily')
+                old_df = old_df.sort_values('시각_utc', ascending=False).reset_index(drop=True)
+
                 old_df.to_feather(save_dir)
                 print(f'{market} => Delta', time.time() - s)
                 continue
 
             else:
+                old_df = old_df.append(latest_df, ignore_index=True)
+
                 while True:
-                    df = self.machine.get_day_candle(market=market, to=last_time)
+                    df = self.machine.get_day_candle(market=market, to=last_time, count=200)
                     df = self.set_columns_dtypes(df, type='daily')
                     last_time = self.get_last_time(df)
                     parsed_last_time = parse(last_time)
 
                     if parsed_last_time <= parsed_first_time:
-                        subset_df = df[df['시각_utc'] > parsed_first_time]
+                        subset_df = df[df['시각_utc'] >= first_time]
                         old_df = old_df.append(subset_df, ignore_index=True)
 
                         old_df = self.set_columns_dtypes(old_df, type='daily')
+                        old_df = old_df.sort_values('시각_utc', ascending=False).reset_index(drop=True)
+
                         old_df.to_feather(save_dir)
                         print(f'{market} => Delta', time.time() - s)
-                        continue
+                        break
 
                     old_df = old_df.append(df, ignore_index=True)                
 
