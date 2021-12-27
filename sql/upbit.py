5c228255a72e8c4c2dc4bd4736423c62287f3e58
SangwonWoo <riddlewoo713@gmail.com>
2021-12-27 22:15:03 +0900

    Add sql folder

---
+++ sql/upbit.py
@@ -0,0 +1,30 @@
+
+SQL_DAILY_CREATE = """
+    CREATE TABLE DailyData (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    시장_종목 TEXT NOT NULL,
+    시각_UTC TEXT NOT NULL,
+    시각_KST TEXT NOT NULL,
+    시가 REAL NOT NULL,
+    고가 REAL NOT NULL,
+    저가 REAL NOT NULL,
+    종가 REAL NOT NULL,
+    누적거래금액 REAL NOT NULL,
+    누적 거래량 REAL NOT NULL);
+"""
+SQL_DAILY_INSERT = """INSERT INTO DailyData{}
+    VALUES ( )
+"""
+SQL_DAILY_UPDATE = """
+"""
+SQL_DAILY_DELETE = """
+"""
+
+SQL_MINUTELY_CREATE = """
+"""
+SQL_MINUTELY_INSERT = """
+"""
+SQL_MINUTELY_UPDATE = """
+"""
+SQL_MINUTELY_DELETE = """
+"""
