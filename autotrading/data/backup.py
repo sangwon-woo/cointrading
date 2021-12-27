51ba9926f33e05f6290b6c568f99b2e1ae89816c
SangwonWoo <riddlewoo713@gmail.com>
2021-12-27 23:44:20 +0900

    Add backup file

--- autotrading/data/backup.py
+++ autotrading/data/backup.py
@@ -9,21 +9,22 @@ def backup_start(source, destination, file_ext):
     count_files = len(data_files)
     print(f'Total File Counts: {count_files}')
 
-    for data_file_name in data_files:
+    for idx, data_file_name in enumerate(data_files):
         load_path = source + f'\\{data_file_name}'
 
-        # if not os.path.isdir(destination):
-        #     os.makedirs(destination)
-        #     print(f'Made dirs => {destination}')
+        if not os.path.isdir(destination):
+            os.makedirs(destination)
+            print(f'Made dirs => {destination}')
 
         save_path = destination + f'\\{data_file_name}'
-        print(load_path, save_path)
-        # if not os.path.exists(save_path):
-        #     shutil.copy(load_path, save_path)
-        #     print(f'Copy {data_file_name} complete')
-        # else:
-        #     os.remove(load_path)
-        #     shutil.copy(load_path, save_path)
+
+        if not os.path.exists(save_path):
+            shutil.copy(load_path, save_path)
+            print(f'Copy {data_file_name} complete({idx+1}/{count_files})')
+        else:
+            os.remove(save_path)
+            shutil.copy(load_path, save_path)
+            print(f'Copy {data_file_name} complete({idx+1}/{count_files})')
 
     print(f'Backup Complete(Spent time : {(time.time() - s):.2f}초)')
 
