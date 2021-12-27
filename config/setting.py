bf9285d65b56346037f9db22e074471965101f12
SangwonWoo <riddlewoo713@gmail.com>
2021-12-27 22:41:44 +0900

    Update setting.py

--- config/setting.py
+++ config/setting.py
@@ -8,6 +8,9 @@ UPBIT_SECRET_KEY = parser.get('upbit', 'secret_key')
 DIR_UPBIT_DATABASE = 'D:\\coin_database\\domestic\\upbit'
 DIR_UPBIT_DAILY_CANDLE = DIR_UPBIT_DATABASE + '\\daily'
 DIR_UPBIT_MINUTELY_CANDLE = DIR_UPBIT_DATABASE + '\\minutely'
+DIR_UPBIT_DATABASE_BACKUP = 'E:\\backup\\coin_database\\domestic\\upbit'
+DIR_UPBIT_DAILY_CANDLE_BACKUP = DIR_UPBIT_DATABASE + '\\daily'
+DIR_UPBIT_MINUTELY_CANDLE_BACKUP = DIR_UPBIT_DATABASE + '\\minutely'
 
 BITHUMB_CONNECT_KEY = parser.get('bithumb', 'connect_key')
 BITHUMB_SECRET_KEY = parser.get('bithumb', 'secret_key')
