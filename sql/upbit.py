257ee2d18eefd515417e715b5562b1724183244e
SangwonWoo <riddlewoo713@gmail.com>
2021-12-28 05:44:10 +0900

    New SQL

--- sql/upbit.py
+++ sql/upbit.py
@@ -1,7 +1,6 @@
 
 SQL_DAILY_CREATE = """
     CREATE TABLE DailyData (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     시장_종목 TEXT NOT NULL,
     시각_UTC TEXT NOT NULL,
     시각_KST TEXT NOT NULL,
@@ -10,10 +9,11 @@ SQL_DAILY_CREATE = """
     저가 REAL NOT NULL,
     종가 REAL NOT NULL,
     누적거래금액 REAL NOT NULL,
-    누적 거래량 REAL NOT NULL);
+    누적거래량 REAL NOT NULL);
 """
-SQL_DAILY_INSERT = """INSERT INTO DailyData{}
-    VALUES ( )
+SQL_DAILY_INSERT = """
+    INSERT INTO DailyData
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
 """
 SQL_DAILY_UPDATE = """
 """
@@ -21,8 +21,20 @@ SQL_DAILY_DELETE = """
 """
 
 SQL_MINUTELY_CREATE = """
+    CREATE TABLE DailyData (
+    시장_종목 TEXT NOT NULL,
+    시각_UTC TEXT NOT NULL,
+    시각_KST TEXT NOT NULL,
+    시가 REAL NOT NULL,
+    고가 REAL NOT NULL,
+    저가 REAL NOT NULL,
+    종가 REAL NOT NULL,
+    누적거래금액 REAL NOT NULL,
+    누적거래량 REAL NOT NULL);
 """
 SQL_MINUTELY_INSERT = """
+    INSERT INTO DailyData
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
 """
 SQL_MINUTELY_UPDATE = """
 """
