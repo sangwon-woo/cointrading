f6323e22d6e048dc1f41585709aedb21a037c9e1
SangwonWoo <riddlewoo713@gmail.com>
2021-12-28 14:58:11 +0900

    Add main

---
+++ main.py
@@ -0,0 +1,14 @@
+import multiprocessing as mp
+
+
+if __name__ == '__main__':
+    while True:
+        break
+    # 데이터를 실시간으로 받기
+
+    # 실시간 데이터를 db와 arr 파일로 저장하기
+
+    # 각 매매 전략의 매매 신호를 확인하기
+
+    # 매매 신호가 발생한 매매 전략 실행하기
+    
