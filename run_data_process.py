b5511ee403e98bd33c04b72fd61261f9ffba00da
SangwonWoo <riddlewoo713@gmail.com>
2022-01-07 11:16:08 +0900

    Add time_print function

--- run_data_process.py
+++ run_data_process.py
@@ -7,6 +7,7 @@ from autotrading.data.collect import collect_data_with_multiprocess
 from autotrading.data.backup import backup_start
 from autotrading.machine.upbit_machine import QuotationAPI
 from config.setting import *
+from util.util import time_print
 
 
 upbit_minutely_files = os.listdir(DIR_UPBIT_MINUTELY_CANDLE)
@@ -42,12 +43,15 @@ if __name__ == '__main__':
         # 1시간 마다 1분봉 다운받기
         print('1분봉 업데이트 전 현재 시간:', now)
         if now.hour != last_hour and now.minute == 0 and (0 <= now.second < 10):
+            s = time.time()
             print('1분봉 업데이트 시작 시간:', now)
             time.sleep(1)
             collect_data_with_multiprocess(
                 'minutely', collector, minutely_market_code_i_have, 9)
             last_hour = now.hour
             print(f'{now} => 1분봉 업데이트 완료')
+            print('1분봉 소요시간', end=' ')
+            time_print(s, time.time())
 
         if now.hour == 9:
             # 매일 오전 9시 30분에 일봉데이터 다운받기
@@ -55,11 +59,14 @@ if __name__ == '__main__':
                 now = datetime.datetime.now()
                 print('일봉 업데이트 전 현재 시간:', now)
                 if now.hour == 9 and now.minute == 30 and (0 <= now.second < 10):
+                    s = time.time()
                     print('일봉 업데이트 시작 시간:', now)
                     time.sleep(1)
                     collect_data_with_multiprocess(
                         'daily', collector, daily_market_code_i_have, 3)
                     print(f'{now} => 일봉 업데이트 완료')
+                    print('일봉 소요시간', end=' ')
+                    time_print(s, time.time())
 
                     # 이후 데이터 백업하기
                     backup_start(DIR_UPBIT_DAILY_CANDLE,
